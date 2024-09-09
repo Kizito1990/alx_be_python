edd50eaa3486ab243e9c7ca8c7fe56c6271345e7
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-09-09 02:42:13 -0700

    Updated the basic_operations.py file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,13 +4,13 @@ number2 = 5
 
 sum_result = number1 +number2
 
-echo(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Addition of {number1} and {number2} is {sum_result}")
 
 
 difference_result = number1 - number2
 
-echo(f"Substraction of {number1} and {number2} is {difference_result}")
+print(f"Substraction of {number1} and {number2} is {difference_result}")
 
 product_result = number1 * number2
 
-echo(f"Multiplication of {number1} and {number2} is {product_results})
+print(f"Multiplication of {number1} and {number2} is {product_results})
