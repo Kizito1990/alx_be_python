be32500e9d602f6170885ebb9e3c1d354673f539
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-09-09 02:32:05 -0700

    Python script that performs basic arithmetic operations with two predefined numbers. The script should do the following

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#!/bin/bash
+number1 = 10
+number2 = 5
+
+sum_result = number1 +number2
+
+echo(f"Addition of {number1} and {number2} is {sum_result}")
+
+
+difference_result = number1 - number2
+
+echo(f"Substraction of {number1} and {number2} is {difference_result}")
+
+product_result = number1 * number2
+
+echo(f"Multiplication of {number1} and {number2} is {product_results})
