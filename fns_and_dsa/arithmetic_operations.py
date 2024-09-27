4595f17d859b9e3e57d1886f987bb2e7c4c3cc80
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-09-27 14:48:37 -0700

    Modified my file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,6 @@
 
 #!/bin/bash
 def perform_operation(num1: float, num2: float, operation: str):
-    num1 = float(input("Enter first number: "))
-    operation = input("Enter the operation 'add', 'subtract', 'divide', 'multiply': ")
-    num2 = float(input("Enter second number:"))
 
     if operation == 'add':
         return num1 + num2
@@ -17,8 +14,5 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 / num2
     else:
         return "INVALID_OPERATION"
-
-result = perform_operation()
-print(f"results: {result}")
 
 
