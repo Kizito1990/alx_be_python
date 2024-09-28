20e1ba6123c7e3ddf0132811f7299c061343cd4c
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-09-28 03:39:55 -0700

    Modified my file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -12,7 +12,7 @@ def display_current_datetime():
 # Part 2: Calculate a Future Date
 def calculate_future_date():
     try:
-        days = int(input("Enter the number of days to add: "))
+        days = int(input("Enter the number of days to add to the current date: "))
         current_date = datetime.now()
         future_date = current_date + timedelta(days=days)
         formatted_future_date = future_date.strftime("%Y-%m-%d")
@@ -27,4 +27,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
+OA
