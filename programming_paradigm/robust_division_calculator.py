3b05256c4252f8aac7cb9eb1e84a55b329256808
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-10-04 03:05:00 -0700

    File

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -8,7 +8,7 @@ def safe_divide(numerator, denominator):
 
         # Perform division
         result = numerator / denominator
-        return f"The result of the division is {result:.1f}"
+        return f"The result of the division is{result:.1f}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
 
