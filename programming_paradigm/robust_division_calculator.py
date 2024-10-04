9916aea50e866428a889bab2f0c5ce675c35cc59
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-10-04 02:31:14 -0700

    Added a file

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -13,10 +13,9 @@ def safe_divide(numerator, denominator):
 
 
     except ZeroDivisionError:
-        return "Error: Cannot divide by zero."
+        return "Cannot divide by zero."
 
     except ValueError:
         return "Error: Please enter numeric values only."
 ~                                                                                                                                                    
-~                                                                                                                                                    
-~                                               
+~                                                                                                                            
