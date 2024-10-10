b4382b1dd4e8fff2deff65d1bf410bf01cee00fd
Azegba Kizito Amandianeze <Azegba Kizito Amandianeze>
2024-10-10 02:59:01 -0700

    Create a Python script named book_class.py. In this script, define a Book class that uses specific magic methods to enhance its functionality.

--- oop/book_class.py
+++ oop/book_class.py
@@ -12,5 +12,5 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book('{self.title} by {self.uthor}, published in {self.year}')"
+        return f"Book('{self.title} by {self.author}, published in {self.year}')"
         
